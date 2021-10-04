c68daeed08660d323a96a9d34dc9dfcaedacffce
jiulou <1665222493@qq.com>
2021-10-04 20:54:12 +0800

    每日一题 166. 分数到小数 482. 密钥格式化

---
+++ src/com/zx/leetcode101_200/l166.java
@@ -0,0 +1,65 @@
+package com.zx.leetcode101_200;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数 。
+ *
+ * 如果小数部分为循环小数，则将循环的部分括在括号内。
+ *
+ * 如果存在多个答案，只需返回 任意一个 。
+ *
+ * 对于所有给定的输入，保证 答案字符串的长度小于 104 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fraction-to-recurring-decimal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class l166 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        long numeratorLong = (long) numerator;
+        long denominatorLong = (long) denominator;
+        if (numeratorLong % denominatorLong == 0) {
+            return String.valueOf(numeratorLong / denominatorLong);
+        }
+
+        StringBuffer sb = new StringBuffer();
+        if (numeratorLong < 0 ^ denominatorLong < 0) {
+            sb.append('-');
+        }
+
+        // 整数部分
+        numeratorLong = Math.abs(numeratorLong);
+        denominatorLong = Math.abs(denominatorLong);
+        long integerPart = numeratorLong / denominatorLong;
+        sb.append(integerPart);
+        sb.append('.');
+
+        // 小数部分
+        StringBuffer fractionPart = new StringBuffer();
+        Map<Long, Integer> remainderIndexMap = new HashMap<Long, Integer>();
+        long remainder = numeratorLong % denominatorLong;
+        int index = 0;
+        while (remainder != 0 && !remainderIndexMap.containsKey(remainder)) {
+            remainderIndexMap.put(remainder, index);
+            remainder *= 10;
+            fractionPart.append(remainder / denominatorLong);
+            remainder %= denominatorLong;
+            index++;
+        }
+        if (remainder != 0) { // 有循环节
+            int insertIndex = remainderIndexMap.get(remainder);
+            fractionPart.insert(insertIndex, '(');
+            fractionPart.append(')');
+        }
+        sb.append(fractionPart.toString());
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = new l166().fractionToDecimal(4, 333);
+        System.out.println(s);
+    }
+}
